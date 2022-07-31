1a7f41a2f1be5d6d24137aacdb3c4f6133246cbd
Garrett <williamhootengarrettgmail.com>
2022-07-31 19:03:12 -0400

    Project Commit (hopefully)

---
+++ Assets/Scripts/testscript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testscript : MonoBehaviour
+{
+    public void button_clicked() {
+
+        Debug.Log("Button clicked!");
+    }
+}
